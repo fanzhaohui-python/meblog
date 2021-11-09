dceeb547f1746be75eb1d5f4da9c8abb443ea7fd
樊朝辉 <fanzhlinux@163.com>
2021-11-09 14:57:32 +0800

    提交代码

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+# 导入Flask类
+from flask import Flask
+
+# 将Flask类的实例 赋值给名为 app的变量，这个实例成为app包的成员
+app = Flask(__name__)
+print("等会谁在试用我：", __name__)
+# 从app包中导入模块routes
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from app import app
+
+
+# 2个路由
+@app.route('/')
+@app.route("/index")
+# 1个视图函数
+def index():
+    return 'Hello,world!'  # 返回一个字符串
