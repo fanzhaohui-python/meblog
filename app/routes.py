4a3ccbc9cd29b06f0461bfe829ad1b5bd6b1d13d
樊朝辉 <fanzhlinux@163.com>
2021-11-10 16:51:19 +0800

    template finish

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
+from flask import render_template
+
 from app import app
 
 
 # 2个路由
 @app.route('/')
 @app.route("/index")
+@app.route("/index/")
 # 1个视图函数
 def index():
-    return 'Hello,world!'  # 返回一个字符串
+    user = {"username": "zhouyj"}
+    posts = [
+        {'author': {'username': 'fanzh'},'body': 'Beautiful day in　Portland!'},
+        {'author': {'username': 'zhouyj'},'body': 'this is aaalll girl!'}
+    ]
+    return render_template('index.html',title = None,user=user,posts = posts) # 返回一个字符串
