cc0794be25d91b24558c38f3efd37f417c660539
樊朝辉 <fanzhlinux@163.com>
2021-11-11 18:31:06 +0800

    form表单

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,7 @@ from app import app
 def index():
     user = {"username": "zhouyj"}
     posts = [
-        {'author': {'username': 'fanzh'},'body': 'Beautiful day in　Portland!'},
-        {'author': {'username': 'zhouyj'},'body': 'this is aaalll girl!'}
+        {'author': {'username': 'fanzh'}, 'body': 'Beautiful day in　Portland!'},
+        {'author': {'username': 'zhouyj'}, 'body': 'this is aaalll girl!'}
     ]
-    return render_template('index.html',title = None,user=user,posts = posts) # 返回一个字符串
+    return render_template('index.html', title=None, user=user, posts=posts)  # 返回一个字符串
