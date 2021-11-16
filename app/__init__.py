7373e0dbd43c95f852dc9db3264b25c8142e0bde
樊朝辉 <fanzhlinux@163.com>
2021-11-16 16:28:05 +0800

    form表单

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 # 导入Flask类
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 # 将Flask类的实例 赋值给名为 app的变量，这个实例成为app包的成员
 app = Flask(__name__)
-print("到低谁在试用我：", __name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+# print("到低谁在试用我：", __name__)
 # 从app包中导入模块routes
-from app import routes
+from app import routes,models
